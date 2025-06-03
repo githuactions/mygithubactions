9c7f17ca6d6aa0fb839b74900c586140bd329022
root <root@ip-172-31-44-71.eu-north-1.compute.internal>
2025-06-03 08:55:19 +0000

    newco

--- index.js
+++ index.js
@@ -1,14 +1,10 @@
 
-const http = require('http');
+// index.js
+const express = require('express');
+const app = express();
+const port = 3000;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
-});
+app.get('/', (req, res) => res.send('Hello World from Node.js!'));
 
-const PORT = 3000;
-server.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}/`);
-});
+app.listen(port, () => console.log(`Server running on port ${port}`));
 
